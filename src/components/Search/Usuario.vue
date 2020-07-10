118f0edb06567544c0f6d4599190354c185e31bb
Enzo <enzoxz.023@gmail.com>
2020-07-10 01:20:30 -0400

    Avancebusqueda

--- src/components/Search/Usuario.vue
+++ src/components/Search/Usuario.vue
@@ -1,23 +1,47 @@
 <template>
   <v-container>
-    <v-card>
-      <v-card-text>{{data.name}}</v-card-text>
-      <v-subheader>{{ moment }}</v-subheader>
-      {{ data }}
+    <v-card width="500px">
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">{{anios}} Years</div>
+          <v-list-item-title class="headline mb-1">{{ data.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ data.city }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-avatar size="100" color="grey">
+          <img src="https://picsum.photos/100/100" />
+        </v-list-item-avatar>
+      </v-list-item>
+      <v-card-actions>
+        <v-btn color="deep-purple darken-4" dark :to="url" block>Hacer reclamo</v-btn>
+      </v-card-actions>
     </v-card>
   </v-container>
 </template>
 
 <script>
-import * as moment from "momentjs";
+import moment from "moment";
 
 export default {
   name: "Usuario",
   props: ["data"],
-  data() {
-    return {
-      hace: moment()
-    };
+  computed: {
+    anios() {
+      const a = moment(this.data.birthDate, "YYYY/MM/DD");
+      const b = moment();
+      return b.diff(a, "years");
+    },
+    url() {
+      const to = "/reclamar/";
+      return to + this.data._id;
+    },
+    ascendente() {
+      // const test = []
+      const data = this.data;
+      data.sort((a, b) => {
+        a.name >= b.name;
+      });
+      return data;
+    }
   }
 };
 </script>
--- src/components/Search/Usuarios.vue
+++ src/components/Search/Usuarios.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
+    <br />
     <h1 class="display-2 text-center">Search</h1>
-    <div v-for="(item, index) in users" :key="index">
-      <Usuario :data="item" />
-    </div>
+    <v-row dense justify="center" no-gutters>
+      <div v-for="(item, index) in users" :key="index">
+        <v-col cols="12" sm="12">
+          <Usuario :data="item" />
+          <v-spacer />
+        </v-col>
+      </div>
+    </v-row>
   </div>
 </template>
 
@@ -23,7 +29,37 @@ export default {
       this.users = docs.data;
     });
   },
-  components: { Usuario }
+  components: { Usuario },
+  computed: {
+    nombreAscendente() {
+      // const test = []
+      const data = this.users.slice();
+      data.sort((a, b) => {
+        const a1 = a.name.toUpperCase();
+        const b1 = b.name.toUpperCase();
+        if (a1 > b1) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+      return data;
+    },
+    nombreDescendente() {
+      // const test = []
+      const data = this.users.slice();
+      data.sort((a, b) => {
+        const a1 = a.name.toUpperCase();
+        const b1 = b.name.toUpperCase();
+        if (a1 < b1) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+      return data;
+    }
+  }
 };
 </script>
 
